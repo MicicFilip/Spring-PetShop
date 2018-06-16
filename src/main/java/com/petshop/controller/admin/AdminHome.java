33b2e52e69429926cc939ed4ce04a2cd04326db6
MicicFilip <micic.filip@protonmail.com>
2018-06-16 16:25:17 +0200

    Products and Items

---
+++ src/main/java/com/petshop/controller/admin/AdminHome.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.petshop.controller.admin;
+
+import com.petshop.model.Customer;
+import com.petshop.model.Product;
+
+import com.petshop.service.CustomerService;
+import com.petshop.service.ProductService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Filip
+ */
+@Controller
+@RequestMapping("/admin")
+public class AdminHome {
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private CustomerService customerService;
+
+   
+
+    @RequestMapping
+    public String adminPage() {
+        return "admin";
+    }
+
+    
+
+    @RequestMapping("/productInventory")
+    public String productInventory(Model model) {
+        List<Product> products = productService.getProductList();
+        model.addAttribute("products", products);
+
+        return "productInventory";
+    }
+
+    @RequestMapping("/customer")
+    public String customerManagerment(Model model) {
+
+        List<Customer> customerList = customerService.getAllCustomers();
+        model.addAttribute("customerList", customerList);
+
+        return "customerManagement";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/admin.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : admin
+    Created on : Jun 16, 2018, 4:24:27 PM
+    Author     : micic
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
